36f22277d6514db9bc247cf61bdbd3c4a24c0b7f
DrOreo002 <bagusanomsurya1@yahoo.com>
2019-04-21 00:14:40 +0800

    CustomSkull changes

--- src/main/java/me/droreo002/oreocore/utils/item/CustomSkull.java
+++ src/main/java/me/droreo002/oreocore/utils/item/CustomSkull.java
@@ -193,11 +193,11 @@ public final class CustomSkull {
      * @return a new ItemStack if its valid section, null otherwise
      */
     @SuppressWarnings("deprecation")
-    public static ItemStack fromSection(ConfigurationSection section, Map<ItemMetaType, TextPlaceholder> placeholder, UUID headOwner) {
+    public static ItemStack fromSection(ConfigurationSection section, TextPlaceholder placeholder, UUID headOwner) {
         Validate.notNull(section, "Section cannot be null!");
         if (!section.contains("material")) throw new NullPointerException("Section must have material key!");
         String material = section.getString("material", "DIRT");
-        int materialDurr = section.getInt("itemID", 0);
+        int materialDurr = section.getInt("itemID", -1);
         int amount = section.getInt("amount", 1);
         boolean glow = section.getBoolean("glow", false);
         String texture = section.getString("texture");
@@ -205,11 +205,9 @@ public final class CustomSkull {
         List<String> lore = (section.getStringList("lore") == null) ? new ArrayList<>() : section.getStringList("lore");
 
         if (placeholder != null) {
-            for (Map.Entry ent : placeholder.entrySet()) {
-                final ItemMetaType editable = (ItemMetaType) ent.getKey();
-                final TextPlaceholder place = (TextPlaceholder) ent.getValue();
+            for (TextPlaceholder place : placeholder.getPlaceholders()) {
 
-                switch (editable) {
+                switch (place.getType()) {
                     case DISPLAY_NAME:
                         for (TextPlaceholder t : place.getPlaceholders()) {
                             if (displayName.contains(t.getFrom())) {
